f1361ffc552a94f3e82e40273e9690bccb27ff9d
claustrophobya <claustrophobya@gmail.com>
2017-11-13 21:23:16 +0200

    updated database and setting the balance

--- app/src/main/java/trackmoney/stefan/claudia/myapplication/Database.java
+++ app/src/main/java/trackmoney/stefan/claudia/myapplication/Database.java
@@ -1,4 +1,5 @@
 package trackmoney.stefan.claudia.myapplication;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,7 +15,7 @@ public class Database extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "TrackMoneyApp";
     public static final String COL_1 ="Balance";
     public static final String COL_2 ="Current Balance";
-    public static final String COL_3 ="Period of time";
+    public static final String COL_3 ="Period_of_time";
     public static final String COL_4 ="weekly";
     public static final String COL_5 ="monthly";
     public static final String COL_6 ="Category";
@@ -26,7 +27,7 @@ public class Database extends SQLiteOpenHelper{
     // creating the database
     public Database(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+
     }
 
     @Override
@@ -41,6 +42,22 @@ public class Database extends SQLiteOpenHelper{
         onCreate (db);
     }
 
+    public boolean insertData (String Balance, String PeriodTime, String weekly, String monthly) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, Balance);
+        contentValues.put(COL_3, PeriodTime);
+        contentValues.put(COL_4, weekly);
+        contentValues.put(COL_5, monthly);
+
+        long result = db.insert(TABLE_NAME,null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+
+        }
+    }
           //  public static final String Balance = "Balance";
             //public static final String Current_Balance = "Current Balance";
           //  public static final String Period_of_time = "Period of time";
@@ -52,4 +69,4 @@ public class Database extends SQLiteOpenHelper{
          //   public static final String Utilities = "Utilities";
         //    public static final String Enterteinment = "Enterteinment";
 
-        }
+
--- app/src/main/java/trackmoney/stefan/claudia/myapplication/PeriodBalance.java
+++ app/src/main/java/trackmoney/stefan/claudia/myapplication/PeriodBalance.java
@@ -1,17 +1,19 @@
 package trackmoney.stefan.claudia.myapplication;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.content.Intent;
 import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.CheckBox;
-
+import android.widget.Toast;
 
 
 public class PeriodBalance extends AppCompatActivity {
@@ -22,8 +24,9 @@ public class PeriodBalance extends AppCompatActivity {
     public TextView currentb;
     public TextView PeriodTime;
     public Spinner periodspiner;
-    public CheckBox weeklycheck;
-    public CheckBox montlycheck;
+    public Button Submitbutton;
+    public TextView weekly;
+    public TextView monthly;
 
 
     @Override
@@ -38,9 +41,31 @@ public class PeriodBalance extends AppCompatActivity {
         currentb = (TextView) findViewById(R.id.currentb);
         PeriodTime = (Button)findViewById(R.id.PeriodTime);
         periodspiner = (Spinner) findViewById(R.id.periodspiner);
-        weeklycheck = (CheckBox) findViewById(R.id.weeklycheck);
-        montlycheck = (CheckBox) findViewById(R.id.montlycheck);
+        Submitbutton = (Button) findViewById(R.id.Submitbutton);
+        AddData();
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PeriodBalance.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.periodspiner));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        periodspiner.setAdapter(myAdapter);
     }
+
+    public void AddData(){
+        Submitbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDB.insertData(iBalance.getText().toString(), PeriodTime.getText().toString(), weekly.getText().toString(), monthly.getText().toString() );
+
+                        if (isInserted = true)
+                            Toast.makeText(PeriodBalance.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(PeriodBalance.this, "Data NOT Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+
 
     //get the spinner from the xml.
     // Spinner dropdown = (Spinner) findViewById(R.id.periodspiner);
